1d92462d4bcebe5ec2a03dea13e933ad73eb4f32
Veeresh <veeresh.d@agaraminfotech.com>
2018-05-09 14:04:11 +0530

    Implemented LiveSummary Service Call

--- NewSportsProject/Controllers/LiveSummaryVC/LiveSummaryVC.h
+++ NewSportsProject/Controllers/LiveSummaryVC/LiveSummaryVC.h
@@ -12,18 +12,54 @@
 
 @interface LiveSummaryVC : UIViewController
 
-@property (nonatomic,strong) IBOutlet UICollectionView * ballsTbl;
+    //Team-A Properties
+@property (strong, nonatomic) IBOutlet UIImageView *teamALogo;
+@property (strong, nonatomic) IBOutlet UILabel *teamANameLbl;
+@property (strong, nonatomic) IBOutlet UILabel *teamAScoreLbl;
+@property (strong, nonatomic) IBOutlet UILabel *teamAOversLbl;
 
+    //Team-B properties
+@property (strong, nonatomic) IBOutlet UIImageView *teamBLogo;
+@property (strong, nonatomic) IBOutlet UILabel *teamBNameLbl;
+@property (strong, nonatomic) IBOutlet UILabel *teamBScoreLbl;
+@property (strong, nonatomic) IBOutlet UILabel *teamBOversLbl;
+
+@property (strong, nonatomic) IBOutlet UILabel *runRateLbl;
+@property (strong, nonatomic) IBOutlet UILabel *matchWonLbl;
+
+@property (nonatomic,strong) IBOutlet UICollectionView * ballsTbl;
 @property (nonatomic,strong) IBOutlet LiveSummaryCell * objcell;
 
-@property (nonatomic,strong) IBOutlet UIImageView * img1;
-@property (nonatomic,strong) IBOutlet UIImageView * img2;
+//Batting Partnership Properties
+@property (nonatomic,strong) IBOutlet UILabel * battingPartnershipLbl;
+@property (strong, nonatomic) IBOutlet UILabel *partnershipRunsLbl;
+@property (strong, nonatomic) IBOutlet UIImageView *battingStrikerImage;
+@property (strong, nonatomic) IBOutlet UILabel *battingStrikerNameLbl;
+@property (strong, nonatomic) IBOutlet UILabel *battingStrikerRunsLbl;
+
+@property (strong, nonatomic) IBOutlet UIImageView *battingNonStrikerImage;
+@property (strong, nonatomic) IBOutlet UILabel *battingNonStrikerNameLbl;
+@property (strong, nonatomic) IBOutlet UILabel *battingNonStrikerRunsLbl;
+
+//Bowling Partnership Properties
+@property (nonatomic,strong) IBOutlet UILabel * bowlingPartnershipLbl;
+@property (strong, nonatomic) IBOutlet UILabel *partnershipOversLbl;
+@property (strong, nonatomic) IBOutlet UIImageView *bowlingStrikerImage;
+@property (strong, nonatomic) IBOutlet UILabel *bowlingStrikerNameLbl;
+@property (strong, nonatomic) IBOutlet UILabel *bowlingStrikerOversLbl;
+
+@property (strong, nonatomic) IBOutlet UIImageView *bowlingNonStrikerImage;
+@property (strong, nonatomic) IBOutlet UILabel *bowlingNonStrikerNameLbl;
+@property (strong, nonatomic) IBOutlet UILabel *bowlingNonStrikerOversLbl;
+
+
 
 @property (nonatomic,strong) IBOutlet UIImageView * img3;
 @property (nonatomic,strong) IBOutlet UIImageView * img4;
 
 
-@property (nonatomic,strong) IBOutlet UILabel * Plbl;
-@property (nonatomic,strong) IBOutlet UILabel * Blbl;
+
+
+
 
 @end
--- NewSportsProject/Controllers/LiveSummaryVC/LiveSummaryVC.m
+++ NewSportsProject/Controllers/LiveSummaryVC/LiveSummaryVC.m
@@ -9,8 +9,13 @@
 #import "LiveSummaryVC.h"
 #import "CustomNavigation.h"
 #import "Config.h"
+#import "WebService.h"
+#import "AppCommon.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
-@interface LiveSummaryVC ()
+@interface LiveSummaryVC () {
+    NSMutableArray *liveScoreArray;
+}
 
 @end
 
@@ -21,46 +26,49 @@
     // Do any additional setup after loading the view.
     
     [self customnavigationmethod];
-    self.img1.layer.masksToBounds = true;
-    self.img1.clipsToBounds = true;
-    self.img1.layer.cornerRadius = self.img1.frame.size.width/2;
-    self.img2.layer.masksToBounds = true;
-    self.img2.clipsToBounds = true;
-    self.img2.layer.cornerRadius = self.img1.frame.size.width/2;
+    self.battingStrikerImage.layer.masksToBounds = true;
+    self.battingStrikerImage.clipsToBounds = true;
+    self.battingStrikerImage.layer.cornerRadius = self.battingStrikerImage.frame.size.width/2;
+    self.battingNonStrikerImage.layer.masksToBounds = true;
+    self.battingNonStrikerImage.clipsToBounds = true;
+    self.battingNonStrikerImage.layer.cornerRadius = self.battingNonStrikerImage.frame.size.width/2;
     
     
-    self.img3.layer.masksToBounds = true;
-    self.img3.clipsToBounds = true;
-    self.img3.layer.cornerRadius = self.img3.frame.size.width/2;
-    self.img4.layer.masksToBounds = true;
-    self.img4.clipsToBounds = true;
-    self.img4.layer.cornerRadius = self.img4.frame.size.width/2;
+    self.bowlingStrikerImage.layer.masksToBounds = true;
+    self.bowlingStrikerImage.clipsToBounds = true;
+    self.bowlingStrikerImage.layer.cornerRadius = self.bowlingStrikerImage.frame.size.width/2;
+    self.bowlingNonStrikerImage.layer.masksToBounds = true;
+    self.bowlingNonStrikerImage.clipsToBounds = true;
+    self.bowlingNonStrikerImage.layer.cornerRadius = self.bowlingNonStrikerImage.frame.size.width/2;
     
     
     UIBezierPath *path = [UIBezierPath new];
     
-    [path moveToPoint:(CGPoint){self.Plbl.frame.size.width-15,0 }];//w0
+    [path moveToPoint:(CGPoint){self.battingPartnershipLbl.frame.size.width-15,0 }];//w0
     [path addLineToPoint:(CGPoint){0, 0}];//00
-    [path addLineToPoint:(CGPoint){15,self.Plbl.frame.size.height }];//0h
-    [path addLineToPoint:(CGPoint){self.Plbl.frame.size.width, self.Plbl.frame.size.height}];//wh20
+    [path addLineToPoint:(CGPoint){15,self.battingPartnershipLbl.frame.size.height }];//0h
+    [path addLineToPoint:(CGPoint){self.battingPartnershipLbl.frame.size.width, self.battingPartnershipLbl.frame.size.height}];//wh20
     
     CAShapeLayer *mask = [CAShapeLayer new];
-    mask.frame = self.Plbl.bounds;
+    mask.frame = self.battingPartnershipLbl.bounds;
     mask.path = path.CGPath;
-    self.Plbl.layer.mask = mask;
+    self.battingPartnershipLbl.layer.mask = mask;
     
     
     UIBezierPath *path1 = [UIBezierPath new];
     
-    [path1 moveToPoint:(CGPoint){self.Blbl.frame.size.width-15,0 }];//w0
+    [path1 moveToPoint:(CGPoint){self.bowlingPartnershipLbl.frame.size.width-15,0 }];//w0
     [path1 addLineToPoint:(CGPoint){0, 0}];//00
-    [path1 addLineToPoint:(CGPoint){15,self.Blbl.frame.size.height }];//0h
-    [path1 addLineToPoint:(CGPoint){self.Blbl.frame.size.width, self.Blbl.frame.size.height}];//wh20
+    [path1 addLineToPoint:(CGPoint){15,self.bowlingPartnershipLbl.frame.size.height }];//0h
+    [path1 addLineToPoint:(CGPoint){self.bowlingPartnershipLbl.frame.size.width, self.bowlingPartnershipLbl.frame.size.height}];//wh20
     
     CAShapeLayer *mask1 = [CAShapeLayer new];
-    mask1.frame = self.Blbl.bounds;
+    mask1.frame = self.bowlingPartnershipLbl.bounds;
     mask1.path = path1.CGPath;
-    self.Blbl.layer.mask = mask1;
+    self.bowlingPartnershipLbl.layer.mask = mask1;
+    
+    [self liveSummaryPostMethodWebService];
+   
 }
 
 
@@ -96,6 +104,73 @@
     
 }
 
+-(void)liveSummaryPostMethodWebService
+{
+    [COMMON loadingIcon:self.view];
+    
+    NSString *URLString =  @"http://csk.agaraminfotech.com/CSK.svc/FETCHPOSTMATCHCOMMENTRY";
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    AFHTTPRequestSerializer *requestSerializer = [AFJSONRequestSerializer serializer];
+    [requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    manager.requestSerializer = requestSerializer;
+    
+    NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
+    [dic    setObject:@"UCC0000274"     forKey:@"COMPETITIONCODE"];
+    [dic    setObject:@"DMSC116D017C2AA4FC420180302113612078"     forKey:@"MATCHCODE"];
+    
+    NSLog(@"parameters : %@",dic);
+    [manager POST:URLString parameters:dic success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"response ; %@",responseObject);
+        liveScoreArray = [NSMutableArray new];
+        liveScoreArray = [responseObject valueForKey:@"lstLiveScore"];
+        [self liveScoreDetails:liveScoreArray];
+        
+        [COMMON RemoveLoadingIcon];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"failed");
+        [COMMON RemoveLoadingIcon];
+    }];
+}
+
+- (void)liveScoreDetails:(NSMutableArray*)liveScore {
+    
+    for (id key in liveScore) {
+        
+        [self.teamALogo sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://csk.agaraminfotech.com/%@", [key valueForKey:@"BATTEAMLOGO"]]] placeholderImage:[UIImage imageNamed:@"no-image"]];
+        
+        self.teamANameLbl.text = [self checkNull:[key valueForKey:@"FIRSTINNINGSSHORTNAME"]];
+        self.teamAScoreLbl.text = [self checkNull:[key valueForKey:@"FIRSTINNINGSSCORE"]];
+        self.teamAOversLbl.text = [NSString stringWithFormat:@"(%@)", [self checkNull:[key valueForKey:@"FIRSTINNINGSOVERS"]]];
+        
+        [self.teamBLogo sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://csk.agaraminfotech.com/%@", [key valueForKey:@"BOWLTEAMLOGO"]]] placeholderImage:[UIImage imageNamed:@"no-image"]];
+        self.teamBNameLbl.text = [self checkNull:[key valueForKey:@"SECONDINNINGSSHORTNAME"]];
+        self.teamBScoreLbl.text = [self checkNull:[key valueForKey:@"SECONDINNINGSSCORE"]];
+        self.teamBOversLbl.text = [NSString stringWithFormat:@"(%@)", [self checkNull:[key valueForKey:@"SECONDINNINGSOVERS"]]];
+        
+        self.runRateLbl.text = [NSString stringWithFormat:@"RR %@ / RRR %@", [self checkNull:[key valueForKey:@"RUNRATE"]], [self checkNull:[key valueForKey:@"REQRUNRATE"]]];
+        
+        self.matchWonLbl.text = [self checkNull:[key valueForKey:@"MATCHSTATUS"]];
+        
+        
+        self.partnershipRunsLbl.text = [NSString stringWithFormat:@"%@ [%@]", [self checkNull:[key valueForKey:@"PARTNERSHIPRUNS"]], [self checkNull:[key valueForKey:@"PARTNERSHIPBALLS"]]];
+        [self.battingStrikerImage sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://csk.agaraminfotech.com/%@", [key valueForKey:@"BATTEAMLOGO"]]] placeholderImage:[UIImage imageNamed:@"no-image"]];
+        self.battingStrikerNameLbl.text = [self checkNull:[key valueForKey:@"STRIKERNAME"]];
+        self.battingStrikerRunsLbl.text = [NSString stringWithFormat:@"%@ [%@]", [self checkNull:[key valueForKey:@"STRIKERTOTALRUNS"]], [self checkNull:[key valueForKey:@"STRIKERTOTALBALLS"]]];
+        
+        
+        [self.battingNonStrikerImage sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://csk.agaraminfotech.com/%@", [key valueForKey:@"BATTEAMLOGO"]]] placeholderImage:[UIImage imageNamed:@"no-image"]];
+        self.battingNonStrikerNameLbl.text = [self checkNull:[key valueForKey:@"NONSTRIKERNAME"]];
+        self.battingNonStrikerRunsLbl.text = [NSString stringWithFormat:@"%@ [%@]", [self checkNull:[key valueForKey:@"NONSTRIKERTOTALRUNS"]], [self checkNull:[key valueForKey:@"NONSTRIKERTOTALBALLS"]]];
+        /*
+        self.partnershipOversLbl.text = [self checkNull:[key valueForKey:@"MATCHSTATUS"]];
+        [self.bowlingStrikerImage sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://csk.agaraminfotech.com/%@", [key valueForKey:@"BATTEAMLOGO"]]] placeholderImage:[UIImage imageNamed:@"no-image"]];
+        self.bowlingStrikerNameLbl.text = [self checkNull:[key valueForKey:@"NONSTRIKERNAME"]];
+        self.bowlingStrikerOversLbl.text = [self checkNull:[key valueForKey:@"MATCHSTATUS"]];
+         */
+    }
+}
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
@@ -112,6 +187,14 @@
     
     return cell;
     
+}
+
+- (NSString *)checkNull:(NSString *)_value
+{
+    if ([_value isEqual:[NSNull null]] || _value == nil || [_value isEqual:@"<null>"] || [_value isEqualToString:@""]) {
+        _value=@"";
+    }
+    return _value;
 }
 
 @end
